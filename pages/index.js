3e01f4fd9a270732c4ed0de625d8837d0d505467
ria sarkar <jitasark@gmail.com>
2023-12-19 00:25:10 -0500

    added more stuff

--- pages/index.js
+++ pages/index.js
@@ -3,32 +3,57 @@ import Card from "../components/Card"
 export default function Home() {
   return (
     <>
-    <h1> Your Feed </h1>
+    <h1> Your Feed 
+
+    </h1>
     <div className="flex ml-10 mt-4 mx-auto gap-4">
       <div className="w-1/3">
       <Card>
-          Navigation
-            <div className="mt-5">
+         <h2> Navigation </h2>
+            <div className="flex mt-5">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" dataSlot="icon" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+            </svg>
+
               Your Feed
             </div>
-            <div className="mt-5">
+            <div className="flex mt-5">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" dataSlot="icon" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
+            </svg>
               Your Ideas
             </div>
-            <div className="mt-5">
+            <div className="flex mt-5">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" dataSlot="icon" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 0 1-2.25 2.25M16.5 7.5V18a2.25 2.25 0 0 0 2.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 0 0 2.25 2.25h13.5M6 7.5h3v3H6v-3Z" />
+            </svg>
               Drafts
             </div>
-            <div className="mt-5">
+            <div className="flex mt-5">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" dataSlot="icon" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
+            </svg>
               Posts
             </div>
         </Card>
       </div>
       <div className="postcard">
         <Card>
-          <form className="textcard">
-            <input className="textinput" type="text" placeholder="Write your next big idea...">
-            </input>
-          </form>
+          <textarea className="area" placeholder="Release your next idea into the world..."> </textarea>
         </Card>
+        <button type="submit" className="btn submit">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 22 22" strokeWidth={1.5} stroke="currentColor" dataSlot="icon" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="m15 11.25-3-3m0 0-3 3m3-3v7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+        </svg>
+        </button>
+        <div className="ideas">
+          <div className="idea">
+            <Card>
+              <div className="thetext">This was the last thing you posted</div>
+              <div className="the author">And here is your author name</div>
+            </Card>
+          </div>
+        </div>
       </div>
     </div>
     </>
