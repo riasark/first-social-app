0f93d5f383de641d9beaec148c87d2c60d7157c3
ria sarkar <jitasark@gmail.com>
2024-02-10 12:50:32 -0500

    fixed up some stuff

--- components/formpost.js
+++ components/formpost.js
@@ -1,12 +1,23 @@
 import Card from "../components/Card";
 import Navigation from "../components/navbar";
+import { useEffect, useState } from "react";
 
-export default function Formpost(){
-    return(
+export default function Formpost({initialContent}){
+  const [content, setContent] = useState("");
+  useEffect(() => {
+      if (initialContent) {
+          setContent(initialContent);
+      }
+  }, [initialContent]);
+  const handleChange = (event) => {
+      setContent(event.target.value);
+  }
+  
+  return(
         <>
         <Card>
           <div className="card">
-            <textarea className="area" placeholder="Release your next idea into the world..."></textarea>
+            <textarea className="area" placeholder="Release your next idea into the world..." value={content} onChange={handleChange}></textarea>
             <button className="submit">
               Submit
             </button>
