94bf81ff1d6bb6d722e555daa6c1672b6154cd2d
Aparajita Sarkar <146043776+riasark@users.noreply.github.com>
2024-02-10 12:38:34 -0500

    Update home.js

--- pages/home.js
+++ pages/home.js
@@ -1,30 +1,64 @@
-import Navigation from "@/components/navbar";
-import Formpost from "@/components/formpost";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import Card from "../components/Card";
+import { useState, useEffect } from "react";
 
-export default function Land(){
-    const router = useRouter();
-    const[draftContent, setDraftContent] = useState("");
+export default function Formpost({initialContent}){
+    const [content, setContent] = useState("");
     useEffect(() => {
-        const {draftContent} = router.query;
-        if (draftContent) {
-            setDraftContent(draftContent)
-        }
-    }, [router.query])
+      if (initialContent) {
+          setContent(initialContent);
+      }
+  }, [initialContent]);
+    const handleChange = (event) => {
+      setContent(event.target.value);
+    }
     return(
-    <>
-        <h1> Your Feed</h1>
-        <div className="flex gap-10">
-            <div className="ml-10">
-                <div className="nav">
-                    <Navigation></Navigation>
-                </div>
-            </div>
-            <div className="postcard">
-                    <Formpost initialContent={draftContent}></Formpost>
-                </div>
+        <>
+        <Card>
+          <div className="card">
+            <textarea className="area" placeHolder= "Release your next idea into the world..." value={content} onChange={handleChange}></textarea>
+            <button className="submit">
+              Submit
+            </button>
+          </div>
+        </Card>
+        <div className="ideas">
+          <div className="idea">
+            <Card>
+              <div className="flex">
+                <div className="flex mr-10">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" dataSlot="icon" className="w-7 h-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 18.75 7.5-7.5 7.5 7.5" />
+                      <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 7.5-7.5 7.5 7.5" />
+                    </svg>
+                    100
+                  </div>
+                  <div className="entry">
+                    This is what I want to say and there is so much to say and I am just trying to test out what it looks like when there is more than it can handle, does it expand into something more?
+                    <div className="authorname">
+                      authorname
+                    </div>
+                  </div>
+              </div>
+            </Card>
+            <Card>
+              <div className="flex">
+                <div className="flex mr-10">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" dataSlot="icon" className="w-7 h-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 18.75 7.5-7.5 7.5 7.5" />
+                      <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 7.5-7.5 7.5 7.5" />
+                    </svg>
+                    10
+                  </div>
+                  <div className="entry">
+                    Hey guys whats up
+                    <div className="authorname">
+                      authorname
+                    </div>
+                  </div>
+              </div>
+            </Card>
+          </div>
         </div>
-    </>
+        </>
     )
 }
